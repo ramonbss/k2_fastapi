0cd00a3acfbb6f27102bc66e6212a9f2b045cfa4
Ramon Brito <ramon.mecatec@gmail.com>
2024-12-23 10:08:59 -0300

    chore: remove not used data

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,4 @@ REMOTE_TOKEN_URL = FAKE_API_URL + "token"
 REMOTE_USER_URL = FAKE_API_URL + "user"
 REMOTE_ADMIN_URL = FAKE_API_URL + "admin"
 
-
-fake_users_db = {
-    "user": {"username": "user", "role": "user", "password": "L0XuwPOdS5U"},
-    "admin": {"username": "admin", "role": "admin", "password": "JKSipm0YH"},
-}
+DATABASE_URL = "sqlite:///auth_data.db"
