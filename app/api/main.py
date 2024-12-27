866f5d8fa999b7fe3452c4d9bf50ba2c02ae39c7
Ramon Brito <ramon.mecatec@gmail.com>
2024-12-27 09:43:59 -0300

    feat: add guidelines when user tries an invalid route

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 from app.api.services.admin_service import initiate_admin_database
 from app.api.services.user_service import initiate_user_database
-
-from app.api.routers import user_routers, admin_routers
+from starlette.exceptions import HTTPException
 
 fast_app = FastAPI()
+
+
+@fast_app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request, exc):
+    if exc.status_code == 404:
+        return JSONResponse(
+            {
+                "detail": "The requested endpoit was not found. Use '/purchase' or '/report'"
+            },
+            status_code=404,
+        )
+    return JSONResponse({"detail": exc.detail}, status_code=exc.status_code)
+
+
+from app.api.routers import user_routers, admin_routers
 
 fast_app.include_router(
     user_routers.user_router, prefix="/purchase", tags=[user_routers.TAG_USERS]
